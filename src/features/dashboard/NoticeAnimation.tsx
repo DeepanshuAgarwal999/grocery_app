d2af53318a30936120e78821850046c82db36f3a
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-19 21:42:26 +0530

    Add NoticeAnimation component for displaying notifications in the dashboard

---
+++ src/features/dashboard/NoticeAnimation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NoticeAnimation = () => {
+    return (
+        <View>
+            <Text>NoticeAnimation</Text>
+        </View>
+    )
+}
+
+export default NoticeAnimation
